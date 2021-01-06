7a84e9545a5ec2e4ffa53c1bb3be7fb78d8cc86f
ricardo-trivino <61172663+ricardo-trivino@users.noreply.github.com>
2021-01-05 20:48:44 -0500

    more JS

--- js/canvas.js
+++ js/canvas.js
@@ -15,122 +15,121 @@ var aZeroOrOne = []
 
 
 var random = (min, max) => {
-  if(typeof max === 'undefined') {
-    return Math.random() * (min[1] - min[0]) + min[0]
-  } else {
-    return Math.random() * (max - min) + min
-  }
+    if (typeof max === 'undefined') {
+        return Math.random() * (min[1] - min[0]) + min[0]
+    } else  {
+        return Math.random() * (max - min) + min
+    }
 }
 
 
 
 class NumberElementConfig {
-  constructor(values, xMinMax, yMinMax, sizeMinMax, speedMinMax) {
-    this.values = values
-    this.x = xMinMax
-    this.y = yMinMax
-    this.size = sizeMinMax
-    this.speed = speedMinMax
-  }
+    constructor(values, xMinMax, yMinMax, sizeMinMax, speedMinMax) {
+        this.values = values
+        this.x = xMinMax
+        this.y = yMinMax
+        this.size = sizeMinMax
+        this.speed = speedMinMax
+    }
 }
 
 class NumberElement {
-  constructor(numberElementConfig) {
-    this.config = numberElementConfig
-    this.spawn()
-  }
-  
-  spawn() {
-    this.value = this.config.values[Math.floor(random(0, 2))]
-    this.size = random(this.config.size)
-    this.speed = random(this.config.speed)
-    this.x = random(this.config.x)
-    this.y = random(this.config.y)
-    
-    this.viewportOffset = this.config.size[1] + 20
-    
-    if(Math.abs(this.speed) < 1) {
-      this.speed += (this.speed < 0 ? -1 : 1)
+    constructor(numberElementConfig) {
+        this.config = numberElementConfig
+        this.spawn()
     }
-    
-    this.setSpawnPosition()
-  }
-  
-  respawn() {
-    this.spawn()
-  }
-  
-  setSpawnPosition() {
-    if(this.speed < 0) {
-      let key = ['x', 'y'][Math.floor(random(0, 2))]
-      let value = key === 'x' ? innerWidth : innerHeight
-      this[key] = value + this.size
-    } else {
-      let key = ['x', 'y'][Math.floor(random(0, 2))]
-      this[key] = 0 - this.size
+
+    spawn() {
+        this.value = this.config.values[Math.floor(random(0, 2))]
+        this.size = random(this.config.size)
+        this.speed = random(this.config.speed)
+        this.x = random(this.config.x)
+        this.y = random(this.config.y)
+
+        this.viewportOffset = this.config.size[1] + 20
+
+        if (Math.abs(this.speed) < 1) {
+            this.speed += (this.speed < 0 ? -1 : 1)
+        }
+
+        this.setSpawnPosition()
     }
-  }
-  
-  isInViewport() {
-    if(
-      this.x < 0 - this.viewportOffset ||
-      this.y < 0 - this.viewportOffset ||
-      this.x > innerWidth + this.viewportOffset ||
-      this.y > innerHeight + this.viewportOffset
-    ) {
-      return false
-    } else {
-      return true
+
+    respawn() {
+        this.spawn()
     }
-  }
-  
-  move() {
-    this.x += this.speed
-    this.y += this.speed
-  }
+
+    setSpawnPosition() {
+        if (this.speed < 0) {
+            let key = ['x', 'y'][Math.floor(random(0, 2))]
+            let value = key === 'x' ? innerWidth : innerHeight
+            this[key] = value + this.size
+        } else {
+            let key = ['x', 'y'][Math.floor(random(0, 2))]
+            this[key] = 0 - this.size
+        }
+    }
+
+    isInViewport() {
+        if (
+            this.x < 0 - this.viewportOffset ||
+            this.y < 0 - this.viewportOffset ||
+            this.x > innerWidth + this.viewportOffset ||
+            this.y > innerHeight + this.viewportOffset
+        ) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    move() {
+        this.x += this.speed
+        this.y += this.speed
+    }
 }
 
 class NumberElementCollector {
 
-  constructor(count = 100, spawnTimeout = 20) {
-    this.numberElements = []
+    constructor(count = 100, spawnTimeout = 20) {
+        this.numberElements = []
 
-    let generator = () => {
-      if(count != this.numberElements.length) {
-        this.numberElements.push(
-          this.generateNumberElement()
+        let generator = () => {
+            if (count != this.numberElements.length) {
+                this.numberElements.push(
+                    this.generateNumberElement()
+                )
+                setTimeout(generator, spawnTimeout)
+            }
+        }
+        generator()
+    }
+
+    get() {
+        return this.numberElements
+    }
+
+    moveAll() {
+        for (let i = 0; i < this.numberElements.length; ++i) {
+            let element = this.numberElements[i]
+            element.move()
+            if (!element.isInViewport()) {
+                element.respawn()
+            }
+        }
+    }
+
+    generateNumberElement() {
+        return new NumberElement(
+            new NumberElementConfig(
+                ['0', '1'], [0, innerWidth], // x
+                [0, innerHeight], // y
+                [15, 50], // size
+                [-12, 12] // speed
+            )
         )
-        setTimeout(generator, spawnTimeout)
-      }
     }
-    generator()
-  }
-  
-  get() {
-    return this.numberElements
-  }
-  
-  moveAll() {
-    for (let i = 0; i < this.numberElements.length; ++i) {
-      let element = this.numberElements[i]
-      element.move()
-      if(!element.isInViewport()) {
-        element.respawn()
-      }
-    }
-  }
-
-  generateNumberElement() {
-    return new NumberElement(
-      new NumberElementConfig(
-        ['0', '1'],
-        [0, innerWidth], // x
-        [0, innerHeight], // y
-        [15, 50], // size
-        [-12, 12] // speed
-      )
-    )
-  }
 }
 
 
@@ -138,34 +137,34 @@ var numberElementCollector = new NumberElementCollector()
 
 
 var draw = () => {
-  ctx.beginPath()
-  ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
-  for (let numberElement of numberElementCollector.get()) {
-    ctx.font = numberElement.size / 1.5 + 'px arial black'
-    ctx.textBaseline = 'bottom'
-    ctx.textAlign = 'left'
-    ctx.fillText(
-      numberElement.value,
-      numberElement.x,
-      numberElement.y
-    )
-  }
-  ctx.fillStyle = "rgba(0, 0, 0, 0.07)"
-  ctx.fill()
-  ctx.lineWidth = 1
-  ctx.strokeStyle = "rgba(0, 0, 0, 0.08)"
-  ctx.stroke()
+    ctx.beginPath()
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+    for (let numberElement of numberElementCollector.get())  {
+        ctx.font = numberElement.size / 1.5 + 'px arial black'
+        ctx.textBaseline = 'bottom'
+        ctx.textAlign = 'left'
+        ctx.fillText(
+            numberElement.value,
+            numberElement.x,
+            numberElement.y
+        )
+    }
+    ctx.fillStyle = "rgba(0, 0, 0, 0.07)"
+    ctx.fill()
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.08)"
+    ctx.stroke()
 }
 
 
 
 var process = () => {
-  width = canvas.width = innerWidth
-  height = canvas.height = innerHeight
+    width = canvas.width = innerWidth
+    height = canvas.height = innerHeight
 
-  numberElementCollector.moveAll()
-  draw()
+    numberElementCollector.moveAll()
+    draw()
 
-  requestAnimationFrame(process)
+    requestAnimationFrame(process)
 }
 process()
